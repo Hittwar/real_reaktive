c38a0a64751e51f8a38d04b7b7d6ccf55b5d7359
hittwar <hittwar@hotmail.com>
2020-07-06 16:22:33 -0500

    users and sites - apps

---
+++ sites/apps.py
@@ -0,0 +1,11 @@
+"""Sites application module"""
+
+from django.apps import AppConfig
+
+
+class SitesConfig(AppConfig):
+    """Sites application settings"""
+
+    name = 'sites'
+    varbose_name = 'Sites'
+    
